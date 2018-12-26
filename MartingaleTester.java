ed9d18e2eeaf083e74caf9808416bd7741457566
TheNerdThing <43583915+TheNerdThing@users.noreply.github.com>
2018-12-26 15:59:27 -0500

    Add files via upload

--- MartingaleTester.java
+++ MartingaleTester.java
@@ -15,92 +15,72 @@ public class MartingaleTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		final int WIN_ON = 49; // player will win if the number rolled is greater than this
-		final int MAX_ROLL = 100; // math.random will select this for a max. 
-		final int STARTING_BANKROLL = 10000;
-		final int STARTING_BET = 5;
-		final int TRIALS_TO_RUN = 10000;
-		final int TABLE_MAX = 1000;
-		final int END_GOAL = (int)(STARTING_BANKROLL * 2);
-		final boolean MARTINGALE = false;
+		
+		final int WIN_ON = 49; 								// player will win if the number rolled is greater than this
+		final int MAX_ROLL = 100; 							// math.random will select this for a max. 
+		final int STARTING_BANKROLL = 10000; 				// the amount of money the player will risk
+		final int STARTING_BET = 5;							// the initial bet
+		final int TRIALS_TO_RUN = 10000;					// number of sims to run. one loop is one sim 
+		final int TABLE_MAX = 1000;							// the max bet a player can bet. if a player exceeds the bet limit he will bet the limit. 
+		final boolean IS_TABLE_MAX = TABLE_MAX != 0; 		// If the max bet is 0 there will be no max bet. 
+		final int END_GOAL = (int)(STARTING_BANKROLL * 2); 	// a sim will run until a player either reaches 0 or his goal. 
+		final boolean MARTINGALE = true;					// true = after we lose a bet, the player's next wager will be 2x his current wager. 
+															// false =we do not change our bet size.
 		
 		int bankRoll = STARTING_BANKROLL; 
 		int currentBet = STARTING_BET;
-		int count = 0;
-		int bustoCount = 0; 
-		int[] data = new int[TRIALS_TO_RUN];
-		int retierCount = 0;
-		int playerwins =0;
-		while(bustoCount < TRIALS_TO_RUN) {
+		int gamesRan = 0;									// this will keep track of how many games were played before the player went bust 
+		int numberOfSims = 0; 
+		int[] data = new int[TRIALS_TO_RUN];				// we will keep track of gamesRan for each sim
+		int playerMetGoal = 0;								// tracks how many times the player reached its goal. 
+		int playerwins =0;									// tracks how many games the player has won
+		
+		while(numberOfSims < TRIALS_TO_RUN) {
+			// start of a sim
 			do {
 				//wager bet
 				bankRoll -= currentBet;
 				// roll dice
 				int roll = (int)((Math.random() * MAX_ROLL)) -1;
-				count ++;
+				gamesRan ++;
 				//did the player win? 
 				if(roll >= WIN_ON) {
 					// add 2x his current bet and set the current bet to starting bet
 					bankRoll += currentBet *2;
 					currentBet = STARTING_BET;
 					playerwins ++;
-					if(currentBet > TABLE_MAX) {
-						currentBet =TABLE_MAX;
+					if(IS_TABLE_MAX && currentBet > TABLE_MAX) {
+						currentBet = TABLE_MAX;
 					}
-//					System.out.print("player has won the wager! ");
 				}else {
 					// on loss, double the bet if we are running using martingale
 					if(MARTINGALE)
 						currentBet = currentBet *2;
-					
-//					System.out.print("player has lost the wager! ");
 				}
-				// print the results
-//				System.out.println(" player has a BR of: " + bankRoll + " after " + count + " trials " + " the next bet will be " + currentBet);
+			// repeat the game while there is still money to gamble, the player can still match his bet, and the player has not met his goal. 
 			}while( bankRoll > 0 && currentBet <= bankRoll && bankRoll <= END_GOAL);
 			if(bankRoll > END_GOAL) {
-				retierCount++;
+				playerMetGoal++;
 			}
-//			System.out.println("the player has busted after " + count + " trials");
-			data[bustoCount] = count;
-			count = 0; 
-			bustoCount ++;
+			// once the player has busted or met his goal, 
+			data[numberOfSims] = gamesRan;
+			gamesRan = 0; 
+			numberOfSims ++;
+			//restart the game
 			currentBet = STARTING_BET;
 			bankRoll = STARTING_BANKROLL;
 		}
 		
 		Arrays.sort(data);
-		int over1k = 0; 
-		int over500 = 0; 
-		int over100 = 0; 
-		int over50 = 0;
-		int under50 = 0;
+		int totalGames =0;		// how many games ran for every sim. 
 		
-		int totalGames =0;
-		
+		// all data is gathered to determine if the player was lucky or unlucky during each run.
 		for(int i = 0; i<data.length; i++) {
 			int use = data[i];
-			System.out.println(use);
-			if(use <= 50 ) {
-				under50++;
-			}
-			else if( use <= 100) {
-				over50 ++;
-			}else if(use <=500) {
-				over100 ++; 
-			}else if(use <=1000) {
-				over500 ++;
-			}else {
-				over1k++;
-			}
 			totalGames += use;
 		}
-		System.out.println(" number of trials over 1,000 	: " + ((double) over1k / TRIALS_TO_RUN));
-		System.out.println(" number of trials over 500   	: " + ((double) over500 / TRIALS_TO_RUN));
-		System.out.println(" number of trials over 100   	: " + ((double) over100 / TRIALS_TO_RUN));
-		System.out.println(" number of trials over 50       : " + ((double) over50 / TRIALS_TO_RUN));
-		System.out.println(" number of trials less than 50  : " + ((double) under50 / TRIALS_TO_RUN));
-		System.out.println(" the player met his goal : " + retierCount + " player retire rate is : " + ((double) retierCount /TRIALS_TO_RUN));
+
+		System.out.println(" the player met his goal : " + playerMetGoal + " player retire rate is : " + ((double) playerMetGoal /TRIALS_TO_RUN));
 		System.out.println("the player's winrate is:  " + (double) playerwins/totalGames + " ( " + playerwins + "  wins total)" + "( " + totalGames + " games total)") ;
 	}
 }
